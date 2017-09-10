5b4a855de325124a02714b8e27f881adf63c94fa
Alishan Khan <alishankhan366@gmail.com>
2017-09-10 16:05:13 +0530

    work on front started

--- backend/models.py
+++ backend/models.py
@@ -3,9 +3,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Tags(models.Model):
+
     description = models.CharField(max_length=2000)
     name = models.CharField(max_length=50)
     dateCreated = models.DateTimeField('date published')
+    slug = models.CharField(max_length=500, null=True)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
 
@@ -16,3 +21,7 @@ class Post(models.Model):
     dateCreated = models.DateTimeField('date published')
     tags = models.ManyToManyField(Tags)
     user = models.ForeignKey(User)
+    image = models.ImageField(upload_to = 'media')
+
+    def __str__(self):
+        return self.title
--- front/views.py
+++ front/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 from backend.models import Post
 # Create your views here.
 
 def index(request):
 
-    posts = Post.object.order_by('dateCreated')[:10]
-
-    return HttpResponse("Test!!!")
+    # posts = Post.objects.order_by('dateCreated')[:10]
+    template = loader.get_template('front/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
 
 def detail(request, slug):
+
     # post = Post.objects.
     return HttpResponse(slug)
